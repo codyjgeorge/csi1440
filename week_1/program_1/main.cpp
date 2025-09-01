67512376cb0364bcba0de431bc738e1f0f252a73
codyjgeorge <codygeorge315@gmail.com>
2025-08-31 21:35:52 -0500

    Opens infection_map.txt, reads/outputs each line, closes map

--- week_1/program_1/main.cpp
+++ week_1/program_1/main.cpp
@@ -28,11 +28,33 @@
  */
 
 #include "InfectionSimulator.h"
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl; // prints "Hello World" to the console
+
+  string infection_map = "infection_map.txt";
+  string line;
+
+  ifstream inputFile(infection_map); // open inputFile: 'infection_map.txt'
+
+  // if infection_map is not open, output 'error', else output 'success'
+
+  if (!inputFile.is_open()) {
+    cout << "Error opening file..." << endl;
+  } else {
+    cout << "Opened successfully!" << endl;
+  }
+
+  // read line from infection_map until EOF
+
+  while (getline(inputFile, line)) {
+    cout << line << endl;
+  }
+
+  inputFile.close(); // close infection_map
+  cout << "infection_map closed suscessfully!" << endl;
   return 0;
 }
